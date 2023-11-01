9c7e8a25c712dd3cd0148be35d5ccc878fa2725c
poldiaz50 <108167418+poldiaz50@users.noreply.github.com>
2023-11-01 02:38:49 -0500

    CRUD Search User Login Flash Messages  User Logout  Restricted Pages

--- base/views.py
+++ base/views.py
@@ -1,5 +1,12 @@
-from django.shortcuts import render
-from .models import Room
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.db.models import Q
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from .models import Room, Topic
+from .forms import RoomForm
 
 # Create your views here.
 
@@ -10,14 +17,95 @@ from .models import Room
 # ]
 
 
+def loginPage(request):
+    
+    if request.user.is_authenticated:
+        return redirect("home")
+    
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, "Usuario no existe¡")
+
+        user = authenticate(request, username=username, password=password)
+        print(username)
+
+        if user is not None:
+            login(request, user)
+            return redirect("home")
+        else:
+            messages.error(request, "Usuario o Contraseña incorrectos")
+
+    context = {}
+    return render(request, "base/login_register.html", context)
+
+
+def logoutUser(request):
+    logout(request)
+    return redirect("home")
+
+
 def home(request):
-    rooms = Room.objects.all()
-    context = {"rooms": rooms}
+    q = request.GET.get("q") if request.GET.get("q") != None else ""
+    rooms = Room.objects.filter(
+        Q(topic__name__icontains=q) | Q(name__icontains=q) | Q(description__icontains=q)
+    )
+
+    topics = Topic.objects.all()
+    room_count = rooms.count()
+
+    context = {"rooms": rooms, "topics": topics, "room_count": room_count}
     return render(request, "base/home.html", context)
 
 
 def room(request, pk):
     room = Room.objects.get(id=pk)
-     
-    context = {'room': room}
+    context = {"room": room}
     return render(request, "base/room.html", context)
+
+
+@login_required(login_url='login')
+def createRoom(request):
+    form = RoomForm()
+    if request.method == "POST":
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("home")
+
+    context = {"form": form}
+    return render(request, "base/room_form.html", context)
+
+
+@login_required(login_url='login')
+def updateRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    form = RoomForm(instance=room)
+    
+    if request.user != room.host:
+        return HttpResponse("No se permite editar una publicacion ajena")
+    
+    if request.method == "POST":
+        form = RoomForm(request.POST, instance=room)
+        if form.is_valid():
+            form.save()
+            return redirect("home")
+    context = {"form": form}
+    return render(request, "base/room_form.html", context)
+
+
+@login_required(login_url='login')
+def deleteRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    
+    if request.user != room.host:
+        return HttpResponse("No se permite eliminar una publicacion ajena")
+    
+    if request.method == "POST":
+        room.delete()
+        return redirect("home")
+    return render(request, "base/delete.html", {"obj": room})
