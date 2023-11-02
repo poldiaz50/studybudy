4ef49fcdcd6154ab4748eff643abe6c82916bad5
poldiaz50 <108167418+poldiaz50@users.noreply.github.com>
2023-11-02 00:12:10 -0500

    User registration Chat Room Messages CRUD

--- base/views.py
+++ base/views.py
@@ -5,7 +5,8 @@ from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
-from .models import Room, Topic
+from django.contrib.auth.forms import UserCreationForm
+from .models import Room, Topic, Message
 from .forms import RoomForm
 
 # Create your views here.
@@ -18,12 +19,13 @@ from .forms import RoomForm
 
 
 def loginPage(request):
-    
+    page = "login"
+
     if request.user.is_authenticated:
         return redirect("home")
-    
+
     if request.method == "POST":
-        username = request.POST.get("username")
+        username = request.POST.get("username").lower()
         password = request.POST.get("password")
 
         try:
@@ -40,13 +42,30 @@ def loginPage(request):
         else:
             messages.error(request, "Usuario o Contraseña incorrectos")
 
-    context = {}
+    context = {"page": page}
     return render(request, "base/login_register.html", context)
 
 
 def logoutUser(request):
     logout(request)
     return redirect("home")
+
+
+def registerPage(request):
+    form = UserCreationForm()
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            login(request, user)
+            return redirect("home")
+        else:
+            messages.error(request, "Ha ocurrido un error en el registro")
+
+    return render(request, "base/login_register.html", {"form": form})
 
 
 def home(request):
@@ -64,11 +83,27 @@ def home(request):
 
 def room(request, pk):
     room = Room.objects.get(id=pk)
-    context = {"room": room}
+    room_messages = room.message_set.all().order_by("-created")
+    participants = room.participants.all()
+
+    if request.method == "POST":
+        messages = Message.objects.create(
+            user=request.user,
+            room=room,
+            body=request.POST.get("body"),
+        )
+        room.participants.add(request.user)
+        return redirect("room", pk=room.id)
+
+    context = {
+        "room": room,
+        "room_messages": room_messages,
+        "participants": participants,
+    }
     return render(request, "base/room.html", context)
 
 
-@login_required(login_url='login')
+@login_required(login_url="login")
 def createRoom(request):
     form = RoomForm()
     if request.method == "POST":
@@ -81,14 +116,14 @@ def createRoom(request):
     return render(request, "base/room_form.html", context)
 
 
-@login_required(login_url='login')
+@login_required(login_url="login")
 def updateRoom(request, pk):
     room = Room.objects.get(id=pk)
     form = RoomForm(instance=room)
-    
+
     if request.user != room.host:
         return HttpResponse("No se permite editar una publicacion ajena")
-    
+
     if request.method == "POST":
         form = RoomForm(request.POST, instance=room)
         if form.is_valid():
@@ -98,14 +133,26 @@ def updateRoom(request, pk):
     return render(request, "base/room_form.html", context)
 
 
-@login_required(login_url='login')
+@login_required(login_url="login")
 def deleteRoom(request, pk):
     room = Room.objects.get(id=pk)
-    
+
     if request.user != room.host:
         return HttpResponse("No se permite eliminar una publicacion ajena")
-    
+
     if request.method == "POST":
         room.delete()
         return redirect("home")
     return render(request, "base/delete.html", {"obj": room})
+
+@login_required(login_url="login")
+def deleteMessage(request, pk):
+    message = Message.objects.get(id=pk)
+
+    if request.user != message.user:
+        return HttpResponse("No se permite eliminar una mensaje ajeno")
+
+    if request.method == "POST":
+        message.delete()
+        return redirect("home")
+    return render(request, "base/delete.html", {"obj": message})
